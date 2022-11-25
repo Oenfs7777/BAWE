13c95a863d5305735512bc6384cb7bb210952c73
Oenfs7777 <oenfs40511@gmail.com>
2022-11-26 00:08:23 +0800

    feat: 搶救回差點被雞哥 bang 不見的專題

--- Assets/Scripts/Player/Ghost.cs
+++ Assets/Scripts/Player/Ghost.cs
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> c400fb8c4bd81642e2a0d591288e1f75fc5ea525
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -39,45 +35,3 @@ public class Ghost : MonoBehaviour
 
     }
 }
-<<<<<<< HEAD
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Ghost : MonoBehaviour
-{
-    public float ghostDelay;
-    private float ghostDelaySeconds;
-    public GameObject ghost;
-    public bool makeGhost = false;
-    void Start()
-    {
-        ghostDelaySeconds = ghostDelay;
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (makeGhost)
-        {
-            if (ghostDelaySeconds > 0)
-            {
-                ghostDelaySeconds -= Time.deltaTime;
-            }
-            else
-            {
-                GameObject currentGhost = Instantiate(ghost, transform.position, transform.rotation);
-                Sprite currentSprite = GetComponent<SpriteRenderer>().sprite;
-                currentGhost.transform.localScale = this.transform.localScale;
-                currentGhost.GetComponent<SpriteRenderer>().sprite = currentSprite;
-                ghostDelaySeconds = ghostDelay;
-                Destroy(currentGhost, 0.1f);
-            }
-        }
-
-    }
-}
->>>>>>> 7a94973f01e0cf33ab82f47554515e21358c8c6d
-=======
->>>>>>> c400fb8c4bd81642e2a0d591288e1f75fc5ea525
