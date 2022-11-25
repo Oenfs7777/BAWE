67f51c457edcb6fa0fb51dcbf6b3d5952731332d
Oenfs7777 <oenfs40511@gmail.com>
2022-11-25 15:53:37 +0800

    feat: 攻擊完善中

--- Assets/Scripts/Player/Attack.cs
+++ Assets/Scripts/Player/Attack.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Attack : MonoBehaviour
+public class Attack : Attack_Way
 {
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/Player/Attack_Way.cs
+++ Assets/Scripts/Player/Attack_Way.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Attack_Way : MonoBehaviour
+public class Attack_Way : Player
 {
     // Start is called before the first frame update
     void Start()
