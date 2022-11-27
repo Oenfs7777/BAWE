761847ac28d31af46d6853b2d20874e2cb7a1a7c
Oenfs7777 <oenfs40511@gmail.com>
2022-11-28 01:24:03 +0800

    feat: 怪物會受到攻擊傷害

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -8,14 +8,16 @@ public class Health : MonoBehaviour
     public int health;
 
     private Animator SA;
+
+    // 受到傷害
     public void TakeDamage(int damage)
     {
         health -= damage;
-       
-        if (health <=0)
+
+        if (health <= 0)
         {
             health = 0;
-           
+
             Debug.Log("Dead!");
 
             SA.SetTrigger("SlimeDead");
@@ -33,10 +35,18 @@ public class Health : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             TakeDamage(5);
         }
     }
-   
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" || collision.tag == "Attack")
+        {
+            TakeDamage(5);
+            Debug.Log("HIT!!");
+        }
+    }
 }
