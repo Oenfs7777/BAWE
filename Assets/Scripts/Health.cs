accdc995a072d563f537395b2e0b633ba3788ad1
Oenfs7777 <oenfs40511@gmail.com>
2022-11-28 03:54:33 +0800

    feat: master 好像不能亂切

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,16 +6,21 @@ public class Health : MonoBehaviour
 {
     // Start is called before the first frame update
     public int health;
-    
+
+    private Animator SA;
+
+    // 受到傷害
     public void TakeDamage(int damage)
     {
         health -= damage;
-       
-        if (health <=0)
+
+        if (health <= 0)
         {
             health = 0;
-           
+
             Debug.Log("Dead!");
+
+            SA.SetTrigger("SlimeDead");
         }
         Healthbar.HealthCurrent = health;
     }
@@ -23,15 +28,25 @@ public class Health : MonoBehaviour
     {
         Healthbar.HealthMax = health;
         Healthbar.HealthCurrent = health;
+
+        SA = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             TakeDamage(5);
         }
     }
-   
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" || collision.tag == "Attack")
+        {
+            TakeDamage(5);
+            Debug.Log("HIT!!");
+        }
+    }
 }
