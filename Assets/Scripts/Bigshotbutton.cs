34a8f0917cabe521c34f9770190c5e4b4ab73514
chickbro <81520619+chickbro@users.noreply.github.com>
2022-11-28 22:32:31 +0800

    技能選擇修改

--- Assets/Scripts/Bigshotbutton.cs
+++ Assets/Scripts/Bigshotbutton.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BtnScript : MonoBehaviour
+public class Bigshotbutton : MonoBehaviour
 {
     int clickedCount = 0;
     private Button clicked1, clicked2;
@@ -12,10 +12,10 @@ public class BtnScript : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if(clickedCount == 2)
+        if (clickedCount == 2)
         {
-            clicked1.transform.position = Vector3.Lerp(clicked1.transform.position, position2,10f );
-            clicked2.transform.position = Vector3.Lerp(clicked2.transform.position, position1,10f );
+            clicked1.transform.position = Vector3.Lerp(clicked1.transform.position, position2, 10f);
+            clicked2.transform.position = Vector3.Lerp(clicked2.transform.position, position1, 10f);
             Invoke("FirstScale", .3f);
         }
 
@@ -23,17 +23,17 @@ public class BtnScript : MonoBehaviour
 
     private void FirstScale()
     {
-        clicked1.transform.name = "btn";
-        clicked2.transform.name = "btn";
-        
+        clicked1.transform.name = "btn2";
+        clicked2.transform.name = "btn2";
+
         clicked1.transform.localScale = new Vector2(1f, 1f);
         clicked2.transform.localScale = new Vector2(1f, 1f);
         clickedCount = 0;
     }
     public void forBtn(Button btn)
     {
-      
-       if(btn.transform.name!= "clicked"&& clickedCount==0)
+
+        if (btn.transform.name != "clicked" && clickedCount == 0)
         {
             clicked1 = btn;
             position1 = btn.transform.position;
@@ -41,7 +41,7 @@ public class BtnScript : MonoBehaviour
             btn.transform.localScale = new Vector2(1.2f, 1.2f);
             clickedCount++;
         }
-       else if(btn.transform.name != "clicked"&&clickedCount == 1)
+        else if (btn.transform.name != "clicked" && clickedCount == 1)
         {
             clicked2 = btn;
             position2 = btn.transform.position;
@@ -49,8 +49,7 @@ public class BtnScript : MonoBehaviour
             btn.transform.localScale = new Vector2(1.2f, 1.2f);
             clickedCount++;
         }
-     
-      
+
+
     }
-   
 }
