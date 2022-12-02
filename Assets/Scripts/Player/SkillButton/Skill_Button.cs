67e76932ac988c3ec8feb77c7ed22d6372c9ab81
nilu412055 <81543527+nilu412055@users.noreply.github.com>
2022-12-02 21:14:54 +0800

    feat: 特效優化

--- Assets/Scripts/Player/SkillButton/Skill_Button.cs
+++ Assets/Scripts/Player/SkillButton/Skill_Button.cs
@@ -12,10 +12,36 @@ public class Skill_Button : MonoBehaviour
 
     }
 
-    public void Skill1()
+    public void Skill1() //Å@¬Þ
     {
         Player_Anim.SetTrigger("IsShield");
     }
+
+    public void Skill2() // ÂêÃì
+    {
+        Player_Anim.SetTrigger("IsChian");
+    }
+
+    public void Skill3() //«ì´_
+    {
+        Player_Anim.SetTrigger("IsCure");
+    }
+
+    public void Skill4() //§ðÀ»buff
+    {
+        Player_Anim.SetTrigger("IsAtkBuff");
+    }
+
+    public void BSkill1() //°{¹q
+    {
+        Player_Anim.SetTrigger("IsThunder");
+    }
+
+    public void BSkil2() //Ã~¤ö
+    {
+        Player_Anim.SetTrigger("IsBeastHit");
+    }
+
 
     public void skill()
     {
