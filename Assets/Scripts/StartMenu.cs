a4faece7601b2aebdc31b3d2385007797695bc2d
Oenfs7777 <oenfs40511@gmail.com>
2022-12-05 01:48:40 +0800

    feat: 技能選擇測試

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -6,20 +6,29 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
-    public void StartMenuButton()
-    {
-        SceneManager.LoadScene("Start");
-    }
+    // Start Scene
     public void StartButton()
     {
         SceneManager.LoadScene("MainScene");
+    }
+    public void TestButton()
+    {
+        SceneManager.LoadScene("TestSkillScene");
     }
     public void ExitButton()
     {
         Application.Quit();
     }
+
+    // Skill Scene
     public void SkillTestButton()
     {
         SceneManager.LoadScene("TestMainScene");
+    }
+
+    // Battle Scene
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("Start");
     }
 }
