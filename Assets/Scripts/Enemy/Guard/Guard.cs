2bbf6678f697a7ccd65ba2ff94894f68ed3e6bfa
nilu412055 <81543527+nilu412055@users.noreply.github.com>
2022-12-04 23:53:44 +0800

    feat: 新增森林守衛戰鬥場景and森林守衛動畫and狀態 尚未完成

---
+++ Assets/Scripts/Enemy/Guard/Guard.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guard : MonoBehaviour
+{
+    private Transform BattleR;
+
+    private float FlipTime;
+    private float FlipTimetarget = 0.8f;
+    public bool flip = true;
+    public Player GetPlayer;
+    public GameObject Arror;
+    private Animator Guard_anim;
+
+    private float ShotRate = 3.0f;
+    private float ShotTime;
+    private int DoWhat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Guard_anim = gameObject.GetComponent<Animator>();
+        BattleR = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (ShotTime > ShotRate)
+        {
+            DoWhat = Random.Range(1, 4);
+            DoAnim();
+            ShotTime = 0;
+        }
+
+        if (FlipTime > FlipTimetarget)
+        {
+            FlipYes();
+            FlipNo();
+
+            FlipTime = 0;
+        }
+        FlipTime += Time.deltaTime;
+        ShotTime += Time.deltaTime;
+    }
+
+    public void TF()
+    {
+        BattleR.position = new Vector3(1, 0, 0);
+        transform.Rotate(0, 180, 0);
+    }
+
+    public void FlipYes()
+    {
+        if (flip == true)
+        {
+            if (GetPlayer.currentPos == Pos.DR || GetPlayer.currentPos == Pos.UR)
+            {
+                transform.Rotate(0, 180, 0);
+                flip = false;
+            }
+        }
+    }
+
+    public void FlipNo()
+    {
+        if (flip == false)
+        {
+            if (GetPlayer.currentPos == Pos.DL || GetPlayer.currentPos == Pos.UL)
+            {
+                transform.Rotate(0, 180, 0);
+                flip = true;
+            }
+        }
+    }
+
+    public void DoAnim()
+    {
+        if (DoWhat == 1)
+        {
+            Guard_anim.SetTrigger("GAtk");
+        }
+        else if (DoWhat == 2)
+        {
+            Guard_anim.SetTrigger("GSBomb");
+        }
+        else if(DoWhat == 3)
+        {
+            Guard_anim.SetTrigger("TAtk");
+        }
+    }
+}
