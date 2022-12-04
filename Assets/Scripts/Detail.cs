fb0542551906935921a738402b9de84bbff6d0e4
chickbro <81520619+chickbro@users.noreply.github.com>
2022-12-04 09:31:14 +0800

    技能敘述

---
+++ Assets/Scripts/Detail.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detail : MonoBehaviour
+{
+    public string skillName;
+    public Sprite skillSprite;
+
+    [TextArea(1, 3)]
+    public string skillDes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
